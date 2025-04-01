66a887a42336338a3808dce0dea0733a2335339d
Alynne Trujillo <160004864+alynnetrujillo@users.noreply.github.com>
2025-04-01 13:00:33 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Planets
-#from models import Person
+from models import db, User, Planets, People, Favorite_People, Favorite_Planets 
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,26 +36,103 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    serialized_users = list(map(lambda x: x.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(serialized_users=serialized_users), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def get_favorites():
+    # get all the people
+    favorite_people = Favorite_People.query.all()
+    favorites_planets = Favorite_Planets.query.all()
+
+    all_favorites = favorite_people + favorites_planets
+
+    serialized_favorites = list(map(lambda x: x.serialize(), all_favorites))
+
+    return jsonify(serialized_favorites), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_people():
+
+    people = User.query.all()
+    serialized_people = list(map(lambda x: x.serialize(), people))
+
+    return jsonify(serialized_people = serialized_people), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_person(people_id):
+    if people_id:
+        person = People.query.get(people_id)
+        if person:
+            return jsonify(person.serialize()), 200
+        else:
+            return jsonify({"msg": "Person not found"}), 404
+
 
 @app.route('/planets', methods=['GET'])
 def get_planets():
 
-        # get all the people
     planets = Planets.query.all()
-
-    # map the results and your list of people inside of the all_people variable
     serialized_planets = list(map(lambda x: x.serialize(), planets))
 
-    return jsonify(serialized_planets = list(map(lambda x: x.serialize(), planets))
-), 200
+    return jsonify(serialized_planets = serialized_planets), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planet(planet_id):
+    if planet_id:
+        planet = Planets.query.get(planet_id)
+        if planet:
+            return jsonify(planet.serialize()), 200
+        else:
+            return jsonify({"msg": "Planet not found"}), 404
+        
+
+@app.route('/favorite/planet/<planet_id>', methods=['POST', 'DELETE'])
+def favorite_planet(planet_id):
+    if request.method == 'POST':
+        planet = Planets.query.get(planet_id)
+        if planet:
+            planet.is_favorite = True
+            db.session.commit()
+            return jsonify({"msg": "Planet marked as favorite"}), 200
+        else:
+            return jsonify({"msg": "Planet not found"}), 404
+
+    elif request.method == 'DELETE':
+        planet = Planets.query.get(planet_id)
+        if planet:
+            planet.is_favorite = False
+            db.session.commit()
+            return jsonify({"msg": "Planet removed from favorites"}), 200
+        else:
+            return jsonify({"msg": "Planet not found"}), 404
+        
+@app.route('/favorite/people/<people_id>', methods=['POST', 'DELETE'])
+def favorite_people(people_id):
+    if request.method == 'POST':
+        person = People.query.get(people_id)
+        if person:
+            person.is_favorite = True
+            db.session.commit()
+            return jsonify({"msg": "Person marked as favorite"}), 200
+        else:
+            return jsonify({"msg": "Person not found"}), 404
+
+    elif request.method == 'DELETE':
+        person = People.query.get(people_id)
+        if person:
+            person.is_favorite = False
+            db.session.commit()
+            return jsonify({"msg": "Person removed from favorites"}), 200
+        else:
+            return jsonify({"msg": "Person not found"}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
